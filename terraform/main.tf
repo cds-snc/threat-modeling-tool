2f4dd334242148779ff94875e960d0d89e06561a
Max Neuvians <max@neuvians.net>
2023-10-17 18:42:57 +0000

    fix: fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
 module "website" {
- source  = "github.com/cds-snc/terraform-modules//simple_static_website?ref=v7.2.0"
+  source = "github.com/cds-snc/terraform-modules//simple_static_website?ref=v7.2.0"
 
- domain_name_source = "threat-modeling.cdssandbox.xyz"
- billing_tag_value  = "threat-modeling-tool"
+  domain_name_source = "threat-modeling.cdssandbox.xyz"
+  billing_tag_value  = "threat-modeling-tool"
 
- providers = {
-   aws           = aws
-   aws.us-east-1 = aws.us-east-1
- }
+  providers = {
+    aws           = aws
+    aws.us-east-1 = aws.us-east-1
+  }
 }
